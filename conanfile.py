4f8315d07c643fad931ec633918e8bb9bb886c7b
Ralf Holly <ralf.holly@approxion.com>
2022-06-17 17:19:09 +0200

    Make package exportable/creatable

--- conanfile.py
+++ conanfile.py
@@ -2,9 +2,12 @@ from conans import ConanFile, CMake, tools, errors
 import os
 
 class ConanHelloWorld(ConanFile):
+    version = "0.0.1"
     name = "conan-hello-world"
+
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    exports_sources = ["CMakeLists.txt", "*.cpp", "*.h"]
 
     def requirements(self):
         pass
